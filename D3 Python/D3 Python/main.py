dc6556586a4fa3012d961e56e333cfbdf8b1aa49
Neomatri <lartigauthiago16@gmail.com>
2023-10-04 11:09:45 -0300

    Programa terminado

---
+++ D3 Python/D3 Python/main.py
@@ -0,0 +1,4 @@
+lista_comidas = ["El pastel de papas", "Las empanadas", "La tarta de verduras", "El guiso de lentejas", "El asado", "Las papas fritas", "La tortilla", "Los ravioles", "Los sorrentinos", "Los ñoquis"]
+
+for x, i in enumerate(lista_comidas):
+    print(f"En la posicion {x} està/n {i}")
