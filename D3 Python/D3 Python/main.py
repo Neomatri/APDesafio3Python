adb11ce9435cd8d2a7b7919085722fa9e77f8980
Neomatri <lartigauthiago16@gmail.com>
2023-10-04 11:12:45 -0300

    Comentario

--- D3 Python/D3 Python/main.py
+++ D3 Python/D3 Python/main.py
@@ -1,4 +1,5 @@
 lista_comidas = ["El pastel de papas", "Las empanadas", "La tarta de verduras", "El guiso de lentejas", "El asado", "Las papas fritas", "La tortilla", "Los ravioles", "Los sorrentinos", "Los ñoquis"]
 
-for x, i in enumerate(lista_comidas):
+#Enumerate se guarda en {x} y cada elemento de la lista se recorre y se guarda en {i}
+for x, i in enumerate(lista_comidas): 
     print(f"En la posicion {x} està/n {i}")
